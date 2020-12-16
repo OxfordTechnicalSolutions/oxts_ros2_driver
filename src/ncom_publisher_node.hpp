26476a12e667d9afede2cfda59e7365a32d0d0b0
smccarthy_oxts <smccarthy@oxts.com>
2020-12-16 09:21:10 +0000

    Tidying and commenting NComPublisherNode class.

--- src/ncom_publisher_node.hpp
+++ src/ncom_publisher_node.hpp
@@ -29,26 +29,21 @@ public:
   NComPublisherNode()
   : Node("ncom_publisher"), count_(0)
   {
-    publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10); // Initialise the publisher with the string message type, topic name "topic", and queue size limit
-    publisher2_ = this->create_publisher<std_msgs::msg::String>("topic2", 10); //
+    // Initialise the publisher with the string message type, topic name 
+    // "topic", and queue size limit
+    publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10); 
+    publisher2_ = this->create_publisher<std_msgs::msg::String>("topic2", 10); 
 
   }
   int ncom_callback(NComRxC* nrx);
 
 private:
-  void timer_callback() // Sets data in message and does the publishing
-  {
-    auto message = std_msgs::msg::String();
-    message.data = "Hello, world! " + std::to_string(count_++);
-    RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
-    publisher_->publish(message);
-    publisher2_->publish(message);
-  }
-  
 
-  rclcpp::TimerBase::SharedPtr timer_;
+  // TODO: Config
   rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
   rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher2_;
+  // TODO: Restructure with different publishers for different messages
+
   size_t count_;
 };
 
