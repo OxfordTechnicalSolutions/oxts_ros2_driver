50596108e02d96144652c88312c8618f72c077ce
smccarthy_oxts <smccarthy@oxts.com>
2020-12-16 09:22:26 +0000

    Developing OxtsDevice class

--- src/oxts_device.cpp
+++ src/oxts_device.cpp
@@ -1,18 +1,13 @@
 #include "oxts_device.hpp"
 
-
-
 int OxtsDevice::handle_ncom()
 {
-    // Read from open socket
+  // Read from open socket
+  std::size_t size = this->udpClient.receive_from(this->buff, 72, this->unitEndpoint);
+  // Add data to decoder
+  NComNewChars(nrx, buff, size); 
+  // Run callback function to publish configured ROS messages
+  this->ncomPublisherNode.ncom_callback(nrx);
 
-
-    // Decode NCom 
-
-
-    // Publish configured ROS messages
-
-
-
-    return 0;
+  return 0;
 }
--- src/oxts_device.hpp
+++ src/oxts_device.hpp
@@ -14,36 +14,49 @@
 
 // Other includes
 #include "ncom_publisher_node.hpp"
-
-//==============================================================================
-// UDP Socket
-//==============================================================================
-
-class UdpSocket
-{
-
-};
-
-
-
-//==============================================================================
-// Oxts Device
-//==============================================================================
+#include "udp_server_client.h"
+/**
+ *Oxts Device
+ */
 class OxtsDevice
 {
-  int port = 3000;
-  std::string ip = "192.168.25.34";
+private:
 
+  // Config
+
+public:
+  std::string unitIp;
+
+  /**
+   * NCom decoder class
+   */
   NComRxC *nrx;
+  /**
+   * ROS2 Publisher Node for NCom data
+   */
   NComPublisherNode ncomPublisherNode;
 
   // Sockets
+  unsigned char buff[1024];
+  networking_udp::client udpClient;
+  boost::asio::ip::udp::endpoint unitEndpoint;
 
   // member functions
-  //OxtsDevice();
+  OxtsDevice()
+  {
+    unitIp = "192.168.25.34";     // Change to 0.0.0.0 once proper setup is made
+    nrx = NComCreateNComRxC();
+    unitEndpoint = boost::asio::ip::udp::endpoint(
+      boost::asio::ip::address::from_string(unitIp), 3000);
+
+    udpClient.set_local_port(3000);
+  }
+
   //~OxtsDevice();
 
-  int handle_ncom();        //!> Look for new NCom packet. Publish ROS messages if there is
+
+  //!> Look for new NCom packet. Decode, then publish ROS messages if there is
+  int handle_ncom();   
 
 
 };
