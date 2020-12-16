98ab802ef02e693592994f222e643dc04419b71d
smccarthy_oxts <smccarthy@oxts.com>
2020-12-16 09:20:16 +0000

    Changing ncom_callback to output ncom position as string message.

--- src/ncom_publisher_node.cpp
+++ src/ncom_publisher_node.cpp
@@ -3,13 +3,15 @@
 
 int NComPublisherNode::ncom_callback(NComRxC* nrx)
 {
-
-
     // Code to publish string - reuse to output some ncom data in string
     auto message = std_msgs::msg::String();
-    message.data = "Hello, world! " + std::to_string(count_++);
+    message.data = "Lat, Long, Alt : " + std::to_string(nrx->mLat) + ", "
+                                + std::to_string(nrx->mLon) + ", "
+                                + std::to_string(nrx->mAlt);
     RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
     publisher_->publish(message);
+
+    // TODO: Add switch statement on different messages to be output
 
     return 0;
 }
