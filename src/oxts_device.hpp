839eb689eb6ba71bea915e283a09039ee76a705c
smccarthy_oxts <smccarthy@oxts.com>
2020-12-16 17:07:48 +0000

    Commenting, tidying.

--- src/oxts_device.hpp
+++ src/oxts_device.hpp
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <iostream>
+#include <memory>
 
 // Boost includes
 #include <boost/asio.hpp>
@@ -24,35 +25,47 @@ private:
   // Config
 
 public:
-
   /**
-   * NCom decoder class
+   * Output rate of NCom on the device. Typically 100Hz
+   */
+  int ncomOutputRate;
+  /**
+   * NCom decoder instance
    */
   NComRxC *nrx;
   /**
    * ROS2 Publisher Node for NCom data
+   * 
+   * @TODO Change to std::shared_ptr<NComPublisherNode>
    */
   NComPublisherNode ncomPublisherNode;
-
-  // Sockets
+  /**
+   * Buffer for UDP data
+   */
   unsigned char buff[1024];
+  /**
+   * UDP Client to receive data from the device. 
+   */
   networking_udp::client udpClient;
+  /**
+   * Endpoint for the udpClient to receive data from 
+   */
   boost::asio::ip::udp::endpoint unitEndpointNCom;
 
   /**
    * Constructor
+   * 
+   * @param deviceIp The IP address of the device to connect to.
    */
   OxtsDevice(std::string deviceIp = "0.0.0.0")
   {
+    ncomOutputRate = 100;
     nrx = NComCreateNComRxC();
 
     unitEndpointNCom = boost::asio::ip::udp::endpoint(
       boost::asio::ip::address::from_string(deviceIp), 3000);
 
   }
-
-  //~OxtsDevice();
-
 
   /**
    *  Look for new NCom packet. Decode, then publish ROS messages if there is
