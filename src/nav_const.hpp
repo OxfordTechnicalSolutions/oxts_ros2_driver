a024b8a3c98112bf664623d44e9d04e0598a9ab7
smccarthy_oxts <smccarthy@oxts.com>
2020-12-17 18:54:02 +0000

    Created nav_const file

--- src/nav_const.hpp
+++ src/nav_const.hpp
@@ -1,9 +1,19 @@
+/**
+ * \file nav_const.hpp
+ * Basic definitions of constants to be used across codebases
+ */
+
 #ifndef NAV_CONST_HPP
 #define NAV_CONST_HPP
+
+#include <math.h>
+
 
 namespace NAV_CONST
 {
     const int SECS2NANOSECS =  1e9;
+    const double DEG2RADS   =  M_PI / 180.0;
+    const double RADS2DEG   =  180.0 / M_PI;
 }
 
 
