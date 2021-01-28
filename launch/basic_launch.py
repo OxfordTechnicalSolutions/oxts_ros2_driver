c3529db8f5bf0050a6524f79c34eb91303fb83f3
Harry Mellor <hmellor@oxts.com>
2021-01-28 09:20:52 +0000

    update launch file for bash ncom specification

--- launch/basic_launch.py
+++ launch/basic_launch.py
@@ -1,7 +1,9 @@
-from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration
 from ament_index_python.packages import get_package_share_directory
 import os
+import yaml
 from pathlib import Path
 
 parameters_file_name = 'ncom_publisher_default_config.yaml'
@@ -9,20 +11,23 @@ parameters_file_name = 'ncom_publisher_default_config.yaml'
 
 def generate_launch_description():
     # get current path and go one level up
-    parameters_file_path = Path(get_package_share_directory('ros-driver'), 'config', parameters_file_name)
-    print(parameters_file_path)
+    share_dir = get_package_share_directory('ros-driver')
+    param_path = Path(share_dir, 'config', parameters_file_name)
+    with open(param_path, 'r') as f:
+        params = yaml.safe_load(f)['ncom_publisher']['ros__parameters']
+
+    ncom = LaunchConfiguration('ncom', default='')
+    params['ncom'] = ncom
+
+    oxts_driver_node = Node(package='ros-driver',
+                            #namespace='unit1',
+                            executable='ncom_publisher',
+                            # name='ncom_publisher',
+                            output='screen',
+                            parameters=[params])
 
     return LaunchDescription([
-        Node(
-            package='ros-driver',
-            #namespace='unit1',
-            executable='ncom_publisher',
-            name='ncom_publisher',
-            output='screen',
-            parameters=[
-                parameters_file_path
-            ],
-        ),
+        oxts_driver_node,
     ])
 
     # Remappings can also be done in this launch file
